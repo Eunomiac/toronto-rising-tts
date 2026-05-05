48e9ce13456bb348435022239c8a2373f084268a
Eunomiac <ryan.o.west@gmail.com>
2026-05-05 14:45:05 -0400

    Tighten TTS state encapsulation and LuaLS typing coverage.
    Remove remaining implicit globals in state/roll cleanup paths, fix a stale gameState reference in player seat handling, and expand local TTS API type stubs so Lua diagnostics recognize common object/component/player methods.

--- types/tts_api.lua
+++ types/tts_api.lua
@@ -6,6 +6,12 @@
 
 ---@class Component
 local Component = {}
+---@param key string
+---@return any
+function Component:get(key) end
+---@param key string
+---@param value any
+function Component:set(key, value) end
 
 ---@class Transform: Component
 local Transform = {}
@@ -36,9 +42,66 @@ local AudioListener = {}
 
 ---@class Object
 local Object = {}
+---@return Vector
+function Object:getPosition() end
+---@return Vector
+function Object:getRotation() end
+---@param componentType string
+---@return Component|nil
+function Object:getComponentInChildren(componentType) end
+---@param componentType string
+---@return Component[]
+function Object:getComponentsInChildren(componentType) end
+---@return Object[]
+function Object:getChildren() end
+---@param notes string
+function Object:setGMNotes(notes) end
+---@return string
+function Object:getGMNotes() end
+---@return string[]
+function Object:getTags() end
+---@param tags string[]
+function Object:setTags(tags) end
 
 ---@class GameObject: Object
 local GameObject = {}
+
+---@class Vector
+local Vector = {}
+
+---@class Color
+local Color = {}
+
+---@class Player
+---@field color string
+---@field steam_name string
+---@field steam_id string|number
+local Player = {}
+---@param mode string
+function Player:setCameraMode(mode) end
+---@param position Vector|table
+function Player:lookAt(position) end
+
+---@class HandsAPI
+---@field playerToPositionMap table<string, any>
+local HandsAPI = {}
+
+---@type HandsAPI
+Hands = Hands
+
+---@param tags string[]
+---@return Object[]
+function getObjectsWithAllTags(tags) end
+
+---@param color string
+---@return table
+function stringColorToRGB(color) end
+
+---@param style string
+---@param color table
+---@param prefix string
+---@param suffix string
+function logStyle(style, color, prefix, suffix) end
 
 return {
   Component = Component,
@@ -52,5 +115,8 @@ return {
   Camera = Camera,
   AudioListener = AudioListener,
   Object = Object,
-  GameObject = GameObject
+  GameObject = GameObject,
+  Vector = Vector,
+  Color = Color,
+  Player = Player
 }
